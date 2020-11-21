2511c7c78037e17c53ca2442a372cb2685f4cb01
Piyush Anand <piyushanand.1221@gmail.com>
2020-11-21 16:48:22 +0530

    Calculate Winner

--- src/components/Game.js
+++ src/components/Game.js
@@ -19,6 +19,13 @@ export default class Game extends React.Component {
     const current = history[history.length - 1];
     const squares = current.squares;
 
+    // * Calculate Winner
+    const winner = calculateWinner(squares);
+    if (winner || squares[i]) {
+      return;
+    }
+
+    // * Set State
     squares[i] = this.state.isXnext ? "X" : "0";
     this.setState({
       history: history.concat({
@@ -45,4 +52,26 @@ export default class Game extends React.Component {
       </div>
     );
   }
+}
+
+function calculateWinner(squares) {
+  const possibilities = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < possibilities.length; i++) {
+    const [a, b, c] = possibilities[i];
+    if (squares[a] && squares[a] === squares[b] && squares[b] === squares[c]) {
+      return squares[a];
+    }
+  }
+
+  return null;
 }
